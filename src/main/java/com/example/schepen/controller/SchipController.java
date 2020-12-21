04d24615b3b1274683490de5746aabc7d16e36e1
kevin <kevin@KEVIN-MBP>
2020-12-21 13:59:58 +0100

    Merge

--- src/main/java/com/example/schepen/controller/SchipController.java
+++ src/main/java/com/example/schepen/controller/SchipController.java
@@ -3,11 +3,9 @@ package com.example.schepen.controller;
 import com.example.schepen.model.Schip;
 import com.example.schepen.repository.SchipRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
